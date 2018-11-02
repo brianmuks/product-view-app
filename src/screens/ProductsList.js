8c8ae66141cb219d9544d49fc48ba79b3657210e
Brian Mukuka <brian.mukuka@zambezii.com>
2018-11-02 17:07:32 +0200

    hide products-view on products-details toggle

--- src/screens/ProductsList.js
+++ src/screens/ProductsList.js
@@ -1,28 +1,46 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 
 import {_PRODUCTS} from './../db/index'
 
-
+const HIDE_PRODUCT_DETAILS = 'scale-out';
+const SHOW_PRODUCT_DETAILS = 'scale-in';
+const HIDE_ON_PHONE_TABLET = 'hide-on-med-and-down';
 function ProductList(){
+
+    const {css,onClick} = useProductOnClick();
+    const getCssDisplay  =useMediaQueryDisplay(css);
+
+    useEffect(()=>{
+        console.log(css)
+    });
+
+
+
+
+
+
     return(
         <div className='col m6 s12'>
-  <ul className="collection">
-        <List />
+
+  <a id="scale-demo" href="#!" className={`btn-floating btn-large scale-transition ${css}`}>
+    <i className="material-icons">add</i>
+  </a>
+  
+
+  <ul className={`collection ${getCssDisplay}`}>
+        <List onClick={onClick}  />
 </ul>
 </div>
     )
 }
 
 
-function List(){
+function List(props){
 
-    useEffect(()=>{
-        console.log('loaded')
-    });
 
     return _PRODUCTS.map((product,key)=>(
     
-    <li  key={key} className="row">
+    <li onClick={props.onClick}  key={key} className="row">
 
         <div className='col s12 center '>
         <img src={product.image} alt="" className="product-img img-responsive" />
@@ -48,14 +66,37 @@ function List(){
             </span>
         </div>
 
-
-    
       </li>
       
     ))
 
 }
 
+function useProductOnClick(){
+    const [isProductDetailsShow,toggleProductDetails] = useState(HIDE_PRODUCT_DETAILS)
+
+   function onClick(){
+    toggleProductDetails(
+        isProductDetailsShow === HIDE_PRODUCT_DETAILS ? SHOW_PRODUCT_DETAILS   : HIDE_PRODUCT_DETAILS
+        
+        )
+
+   }
+
+    return {
+        onClick:onClick,
+        css:isProductDetailsShow
+    }
+
+}
+
+function useMediaQueryDisplay(display){
+
+    useEffect(()=>{
+      
+    },[display])
+    return display !== HIDE_PRODUCT_DETAILS ? HIDE_ON_PHONE_TABLET : null
+}
 
 
 
